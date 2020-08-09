da65290af73e4d55f6182e36c2252f82f0b5c52e
Simon Michael <simon@joyful.com>
2020-08-09 12:56:06 -0700

    disable "smart" punctuation in sphinx doc rendering

--- conf.py
+++ conf.py
@@ -157,6 +157,8 @@ html_context = {
 
 master_doc = 'sitemap'
 
+smartquotes = False
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
